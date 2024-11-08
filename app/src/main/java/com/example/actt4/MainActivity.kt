f7a7bd0ac706715553809ba85a13082538a8b426
PrinzBrylle <117422222+PrinzBrylle@users.noreply.github.com>
2024-11-08 16:50:59 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/actt4/MainActivity.kt
+++ app/src/main/java/com/example/actt4/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.act4.R
+import com.example.actt4.R
 import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
